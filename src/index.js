8afd1d8ec03fcebeb81b3eb02bf87f635ea4568b
Chloé *Matcha* Desoutter <chloe.desoutter@bluenove.com>
2020-05-19 16:23:58 +0200

    Support extra data, HTML editorial description

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-let poly = require("preact-cli/lib/lib/webpack/polyfills");
+// let poly = require('preact-cli/lib/lib/webpack/polyfills');
 import './style';
 import habitat from 'preact-habitat';
 import { config as appConfig } from './config';
-import definition from './i18n/fr-fr.json';
+import definitions from './i18n';
 import { IntlProvider } from 'preact-i18n';
 import { BrightMirror } from './Components/BrightMirror';
 
 
-
 const IntlApp = ({ children, ...props }) => {
   const runtimeConfig = { ...appConfig, ...props };
-  return (<IntlProvider definition={definition}>
+  const language = runtimeConfig.language ? runtimeConfig.language : 'fr-nnv';
+  return (<IntlProvider definition={definitions[language]}>
     <BrightMirror
       newPostEndpoint={runtimeConfig.api.newPostEndpoint}
       readPostEndpoint={runtimeConfig.api.readPostEndpoint}
+      postExtraData={runtimeConfig.postExtraData}
       topic={runtimeConfig.editorial.topic}
       instructions={runtimeConfig.editorial.instructions}
       brightMirrorIndexPage={runtimeConfig.editorial.brightMirrorIndexPage}

6841a1c82dfdd44a5c5187e9210c11287f2f5942
Chloé "Matcha" Desoutter <chloe.desoutter@bluenove.com>
2021-01-22 14:57:07 +0100

    php file: auto versioning

--- bright-mirror.php
+++ bright-mirror.php
@@ -7,7 +7,7 @@
  * Author URI:      https://gitlab.com/ChloeTigre
  * Text Domain:     bright-mirror
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         {{version}}
  *
  * @package         Bright_Mirror
  */
